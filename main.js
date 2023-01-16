68031440b6eef8175e548b5d0d6ad980ce8d4e9e
Hillel Wasserman <hillelwasser95@gmail.com>
2023-01-16 08:44:24 +0200

    16.1.23

---
+++ main.js
@@ -0,0 +1,29 @@
+
+function openMenu () {
+    let links = document.getElementById('downMenu');
+    if(links===none) {
+        links.display=== block ; 
+    }
+    else {
+        links.display === none;
+    }
+}
+
+const btnMenu = document.getElementById('menuBtn');
+btnMenu.addEventListener('click',event => {
+    event.preventDefault();
+    function openMenu();
+})
+
+
+
+
+
+
+// const goodbyeBtn = document.getElementById('Goodbye');
+// goodbyeBtn.addEventListener('click', event1 => {
+//   event1.preventDefault();
+//   alert('good night!!');
+// });
+
+// chooseTheBtn();
